c41e4ed2c3bd8ca58a1832e329307cccf7602cc8
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-03-24 15:00:48 +1300

    Live chat is implemented and users can set their nicknames

--- live_chat/index.js
+++ live_chat/index.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var people = {};
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+    socket.on('join', function(name){
+        console.log(name + " Has joined");
+        people[socket.id] = name;
+        socket.emit('update', 'You have connected to the chat');
+        io.emit('update', name + ' has joined the chat');
+
+    });
+
+    socket.on('send', function(msg){
+        io.emit('chat', people[socket.id], msg);
+    });    
+
+    socket.on('disconnect', function(){
+        io.emit('update', people[socket.id] + ' has left the chat');
+        delete people[socket.id];
+    });
 });
